a9d5e8f204239f8b09bfde41e9ffd8532d758e8b
Dawid Spiechowicz <spiechu@gmail.com>
2021-01-04 18:24:15 +0100

    port ENV

--- main.go
+++ main.go
@@ -1,83 +1,90 @@
 package main
 
 import (
-    "fmt"
-    "golang.org/x/net/websocket"
-    "log"
-    "math/rand"
-    "net/http"
-    "sync"
-    "time"
+	"fmt"
+	"golang.org/x/net/websocket"
+	"log"
+	"math/rand"
+	"net/http"
+	"os"
+	"sync"
+	"time"
 )
 
 func getWsHandler(pool *Pool) websocket.Handler {
-    return func(ws *websocket.Conn) {
-        var clientID int
+	return func(ws *websocket.Conn) {
+		var clientID int
 
-    OUTER:
-        for {
-            clientID = rand.Intn(1000000)
+	OUTER:
+		for {
+			clientID = rand.Intn(1000000)
 
-            if clientID == 0 {
-                continue OUTER
-            }
+			if clientID == 0 {
+				continue OUTER
+			}
 
-            for c := range pool.Clients {
-                if clientID == c.ID {
-                    continue OUTER
-                }
-            }
+			for c := range pool.Clients {
+				if clientID == c.ID {
+					continue OUTER
+				}
+			}
 
-            break OUTER
-        }
+			break OUTER
+		}
 
-        client := &Client{
-            ID:   clientID,
-            Name: fmt.Sprintf("Anonymous%d", clientID),
-            Conn: ws,
-            Pool: pool,
-            RW:   &sync.RWMutex{},
-        }
+		client := &Client{
+			ID:   clientID,
+			Name: fmt.Sprintf("Anonymous%d", clientID),
+			Conn: ws,
+			Pool: pool,
+			RW:   &sync.RWMutex{},
+		}
 
-        pool.Register <- client
+		pool.Register <- client
 
-        client.Read()
-    }
+		client.Read()
+	}
 }
 
 func main() {
-    serverStartedAt := time.Now()
+	serverStartedAt := time.Now()
 
-    pool := NewPool()
+	pool := NewPool()
 
-    go pool.Start()
+	go pool.Start()
 
-    schedule := Schedule{
-        Pool: pool,
-    }
+	schedule := Schedule{
+		Pool: pool,
+	}
 
-    schedule.Start(5*time.Second, "System health", func(s *Schedule) {
-        ms := PrintMemUsage()
+	schedule.Start(5*time.Second, "System health", func(s *Schedule) {
+		ms := PrintMemUsage()
 
-        ms.ServerStartedAt = serverStartedAt.Format(time.RFC822)
-        ms.ActiveUsersCount = len(s.Pool.Clients)
+		ms.ServerStartedAt = serverStartedAt.Format(time.RFC822)
+		ms.ActiveUsersCount = len(s.Pool.Clients)
 
-        stringified, err := ms.ToJSON()
-        if err != nil {
-            fmt.Println(err)
-            return
-        }
+		stringified, err := ms.ToJSON()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
 
-        s.Pool.Broadcast <- OutgoingMessage{
-            Type:   BackgroundSystemInfo,
-            Body:   stringified,
-            Sender: "",
-        }
-    })
+		s.Pool.Broadcast <- OutgoingMessage{
+			Type:   BackgroundSystemInfo,
+			Body:   stringified,
+			Sender: "",
+		}
+	})
 
-    http.Handle("/", getWsHandler(pool))
+	http.Handle("/", getWsHandler(pool))
 
-    if err := http.ListenAndServe(":1234", nil); err != nil {
-        log.Fatal("ListenAndServe:", err)
-    }
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "1234"
+	}
+
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
 }
--- monitoring.go
+++ monitoring.go
@@ -1,56 +1,51 @@
 package main
 
 import (
-    "encoding/json"
-    "fmt"
-    "runtime"
+	"encoding/json"
+	"fmt"
+	"runtime"
 )
 
 type MonitoringStats struct {
-    Alloc            uint64
-    TotalAlloc       uint64
-    HeapAlloc        uint64
-    Sys              uint64
-    NumGC            uint32
-    ServerStartedAt  string
-    ActiveUsersCount int
+	Alloc            uint64
+	TotalAlloc       uint64
+	HeapAlloc        uint64
+	Sys              uint64
+	NumGC            uint32
+	ServerStartedAt  string
+	ActiveUsersCount int
 }
 
 func (m *MonitoringStats) ToJSON() (stringified string, err error) {
-    b, err := json.Marshal(m)
+	b, err := json.Marshal(m)
 
-    if err != nil {
-        fmt.Println(err)
+	if err != nil {
+		fmt.Println(err)
 
-        return "", err
-    }
+		return "", err
+	}
 
-    fmt.Println(string(b))
+	fmt.Println(string(b))
 
-    return string(b), nil
+	return string(b), nil
 }
 
 func PrintMemUsage() MonitoringStats {
-    var m runtime.MemStats
+	var m runtime.MemStats
 
-    runtime.ReadMemStats(&m)
+	runtime.ReadMemStats(&m)
 
-    // For info on each, see: https://golang.org/pkg/runtime/#MemStats
-    fmt.Printf("Alloc = %v MiB", bToMb(m.Alloc))
-    fmt.Printf("\tTotalAlloc = %v MiB", bToMb(m.TotalAlloc))
-    fmt.Printf("\tHeapAlloc  = %v MiB", bToMb(m.HeapAlloc))
-    fmt.Printf("\tSys = %v MiB", bToMb(m.Sys))
-    fmt.Printf("\tNumGC = %v\n", m.NumGC)
+	// For info on each, see: https://golang.org/pkg/runtime/#MemStats
 
-    return MonitoringStats{
-        Alloc:      bToMb(m.Alloc),
-        TotalAlloc: bToMb(m.TotalAlloc),
-        HeapAlloc:  bToMb(m.HeapAlloc),
-        Sys:        bToMb(m.Sys),
-        NumGC:      m.NumGC,
-    }
+	return MonitoringStats{
+		Alloc:      bToMb(m.Alloc),
+		TotalAlloc: bToMb(m.TotalAlloc),
+		HeapAlloc:  bToMb(m.HeapAlloc),
+		Sys:        bToMb(m.Sys),
+		NumGC:      m.NumGC,
+	}
 }
 
 func bToMb(b uint64) uint64 {
-    return b / 1024 / 1024
+	return b / 1024 / 1024
 }
